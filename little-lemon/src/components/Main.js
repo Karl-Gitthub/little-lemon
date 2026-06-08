28fa04923aad2739bab31d698bff55e2b385e50a
Karl <karlcaparal05@gmail.com>
2026-06-09 00:58:46 +0800

    Forgot the component main

---
+++ little-lemon/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <main>
+            <h1>Welcome to Little Lemon</h1>
+            <p>Experience the best of Mediterranean cuisine in the heart of Chicago. Our menu features a variety of dishes made with fresh, locally sourced ingredients. Whether you're in the mood for a light salad or a hearty pasta, we have something for everyone. Join us for a memorable dining experience!</p>     
+        </main>
+    )
+}
+
+export default Main
