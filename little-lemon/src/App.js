f370d11f940f0af640d53546404b5ab76cf19cc0
Karl <karlcaparal05@gmail.com>
2026-06-09 00:54:46 +0800

    Done Semantic Website Structure

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,17 @@
-
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ little-lemon/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <img src={logo} alt="Little Lemon" />
+
+            <nav aria-label="Footer navigation">
+                <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order">Order online</a></li>
+                </ul>
+            </nav>
+
+            <address>
+                <p>123 Example Street, Chicago</p>
+                <p><a href="tel:+11234567890">(123) 456-7890</a></p>
+                <p><a href="mailto:hello@littlelemon.com">hello@littlelemon.com</a></p>
+            </address>
+
+            <ul aria-label="Social media links">
+                <li><a href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a></li>
+                <li><a href="https://instagram.com" target="_blank" rel="noreferrer">Instagram</a></li>
+                <li><a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
